a572d7c96fd2d9be9234cb3846b206247be0a7c9
omi-nywusa <120270101+omi-nywusa@users.noreply.github.com>
2022-12-10 14:44:24 -0500

    Delete dist directory

--- dist/assets/SyButton.921e2d65.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,a as n,q as s}from"./index.bee75b83.js";const _={},c={class:"sy-button"};function a(e,r){return o(),n("button",c,[s(e.$slots,"default",{},void 0,!0)])}const u=t(_,[["render",a],["__scopeId","data-v-580181b5"]]);export{u as S};
